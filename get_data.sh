e58bd692c169232e529988038496c28fdcda6774
Charles Lu <charleslu@live.com>
2017-06-23 03:31:25 +0000

    fix path

--- get_data.sh
+++ get_data.sh
@@ -1,7 +1,7 @@
 sudo apt install unzip python3 python3-pip
 pip3 install -r requirements.txt
-mkdir ~/datasets
-cd ~/datasets
+mkdir ../datasets
+cd ../datasets
 wget http://openi.nlm.nih.gov/imgs/collections/ChinaSet_AllFiles.zip
 wget http://openi.nlm.nih.gov/imgs/collections/NLM-MontgomeryCXRSet.zip
 unzip ChinaSet_AllFiles.zip
